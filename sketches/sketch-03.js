0a4f279eda5dd758284c656a9d08884e746ef571
ShannyxMP <shannyxmp@gmail.com>
2024-04-20 12:30:47 +1000

    code added to sketch-03.js

--- sketches/sketch-03.js
+++ sketches/sketch-03.js
@@ -1,35 +1,50 @@
 const canvasSketch = require("canvas-sketch");
+const random = require("canvas-sketch-util/random");
 
 const settings = {
   dimensions: [1080, 1080],
 };
 
-const sketch = () => {
+const sketch = ({ context, width, height }) => {
+  const agents = [];
+
+  for (let i = 0; i < 40; i++) {
+    const x = random.range(0, width);
+    const y = random.range(0, height);
+
+    agents.push(new Agent(x, y));
+  }
+
   return ({ context, width, height }) => {
     context.fillStyle = "white";
     context.fillRect(0, 0, width, height);
 
-    const pointA = new Point(800, 400, 10);
-    const pointB = new Point(300, 700, 10);
-
-    context.beginPath();
-    context.arc(pointA.x, pointA.y, pointA.radius, 0, Math.PI * 2);
-    context.fillStyle = "black";
-    context.fill();
-
-    context.beginPath();
-    context.arc(pointB.x, pointB.y, pointB.radius, 0, Math.PI * 2);
-    context.fillStyle = "black";
-    context.fill();
+    agents.forEach((agent) => {
+      agent.draw(context);
+    });
   };
 };
 
 canvasSketch(sketch, settings);
 
 class Point {
-  constructor(x, y, radius) {
+  constructor(x, y) {
     this.x = x;
     this.y = y;
-    this.radius = radius;
+  }
+}
+
+class Agent {
+  constructor(x, y) {
+    this.pos = new Point(x, y);
+    this.radius = 10;
+  }
+
+  draw(context) {
+    context.fillStyle = "black";
+
+    context.beginPath();
+    context.arc(this.pos.x, this.pos.y, this.radius, 0, Math.PI * 2);
+    context.fill();
   }
 }
