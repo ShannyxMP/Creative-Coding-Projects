471c991954d785266f54ad12c7d9e79b4f9f734d
ShannyxMP <shannyxmp@gmail.com>
2024-04-20 18:14:07 +1000

    code added to sketch-03.js

--- sketches/sketch-03.js
+++ sketches/sketch-03.js
@@ -32,6 +32,19 @@ const sketch = ({ context, width, height }) => {
     context.fillStyle = "white";
     context.fillRect(0, 0, width, height);
 
+    for (let i = 0; i < agents.length; i++) {
+      const agent = agents[i];
+
+      for (let j = i + 1; j < agents.length; j++) {
+        const other = agents[j];
+
+        context.beginPath();
+        context.moveTo(agent.pos.x, agent.pos.y);
+        context.lineTo(other.pos.x, other.pos.y);
+        context.stroke();
+      }
+    }
+
     // Update and draw each agent
     agents.forEach((agent) => {
       agent.update();
