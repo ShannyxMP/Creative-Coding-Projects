fe3c0001d271c445cb620c4a23ef31852db06bcb
ShannyxMP <shannyxmp@gmail.com>
2024-07-05 17:42:08 +1000

    code added to sketch-04.js

--- sketches/sketch-04.js
+++ sketches/sketch-04.js
@@ -10,40 +10,42 @@ const settings = {
 
 // Parameters for controlling the sketch
 const params = {
-  cols: 10, // Number of columns in the grid
-  rows: 10, // Number of rows in the grid
+  Columns: 10, // Number of columns in the grid
+  Rows: 10, // Number of Rows in the grid
   scaleMin: 1, // Minimum scale of the lines
   scaleMax: 30, // Maximum scale of the lines
-  freq: 0.001, // Frequency of the noise
-  amp: 0.2, // Amplitude of the noise
+  Frequency: 0.001, // Frequency of the noise
+  Amplitude: 0.2, // Amplitude of the noise
   frame: 0, // Current frame number
   animate: true, // Whether to animate or not
   lineCap: "butt", // Line cap style (butt, round, square)
+  strokeStyle: "#FFFFFF",
+  fillStyle: "#000000",
 };
 
 const sketch = () => {
   return ({ context, width, height, frame }) => {
     // Setting the background color of the canvas to white
-    context.fillStyle = "white";
+    context.fillStyle = params.fillStyle;
     context.fillRect(0, 0, width, height);
 
     // Defining the grid dimensions
-    const cols = params.cols;
-    const rows = params.rows;
-    const numCells = cols * rows;
+    const Columns = params.Columns;
+    const Rows = params.Rows;
+    const numCells = Columns * Rows;
 
     // Calculating the dimensions and margins of the grid cells
     const gridw = width * 0.8;
     const gridh = height * 0.8;
-    const cellw = gridw / cols;
-    const cellh = gridh / rows;
+    const cellw = gridw / Columns;
+    const cellh = gridh / Rows;
     const margx = (width - gridw) * 0.5;
     const margy = (height - gridh) * 0.5;
 
     // Looping through each cell in the grid
     for (let i = 0; i < numCells; i++) {
-      const col = i % cols; // Calculate column index (x-axis) of the current cell
-      const row = Math.floor(i / cols); // Calculate row index (y-axis) of the current cell
+      const col = i % Columns; // Calculate column index (x-axis) of the current cell
+      const row = Math.floor(i / Columns); // Calculate row index (y-axis) of the current cell
 
       // Calculate the position and size of the current cell
       const x = col * cellw;
@@ -54,10 +56,10 @@ const sketch = () => {
       // Use frame as the third dimension of noise when animation is enabled, otherwise, use the fixed frame value from params
       const f = params.animate ? frame : params.frame; // <=TERNARY OPERATOR -- Here you want the value of 'f' to be frame when parameter animate is true; 'params.frame' when parameter animate is false
 
-      // const n = random.noise2D(x + frame * 10, y, params.freq); // Here you are incrementing the value of 'x' in the noise2D function -> this results in the movement of the panels from right to left
-      const n = random.noise3D(x, y, f * 10, params.freq); // Here you are using the frame as the third dimension
+      // const n = random.noise2D(x + frame * 10, y, params.Frequency); // Here you are incrementing the value of 'x' in the noise2D function -> this results in the movement of the panels from right to left
+      const n = random.noise3D(x, y, f * 10, params.Frequency); // Here you are using the frame as the third dimension
 
-      const angle = n * Math.PI * params.amp; // Will produce number from -180 to 180degrees
+      const angle = n * Math.PI * params.Amplitude; // Will produce number from -180 to 180degrees
       const scale = math.mapRange(n, -1, 1, params.scaleMin, params.scaleMax); // ALTERNATIVE: const scale = ((n + 1) / 2) * 30; OR const scale = (n * 0.5 + 0.5) * 30;
 
       // Save the current transformation state
@@ -75,6 +77,7 @@ const sketch = () => {
 
       // Draw a horizontal line in the center of the cell
       context.beginPath();
+      context.strokeStyle = params.strokeStyle;
       context.moveTo(w * -0.5, 0);
       context.lineTo(w * 0.5, 0);
       context.stroke();
@@ -93,16 +96,20 @@ const createPane = () => {
   folder.addInput(params, "lineCap", {
     options: { butt: "butt", round: "round", square: "square" },
   });
-  folder.addInput(params, "cols", { min: 2, max: 50, step: 1 });
-  folder.addInput(params, "rows", { min: 2, max: 50, step: 1 });
+  folder.addInput(params, "Columns", { min: 2, max: 50, step: 1 });
+  folder.addInput(params, "Rows", { min: 2, max: 50, step: 1 });
   folder.addInput(params, "scaleMin", { min: 1, max: 100 });
   folder.addInput(params, "scaleMax", { min: 1, max: 100 });
 
   folder = pane.addFolder({ title: "Noise" });
-  folder.addInput(params, "freq", { min: -0.01, max: 0.01 });
-  folder.addInput(params, "amp", { min: 0, max: 1 });
+  folder.addInput(params, "Frequency", { min: -0.01, max: 0.01 });
+  folder.addInput(params, "Amplitude", { min: 0, max: 1 });
   folder.addInput(params, "frame", { min: 0, max: 999 });
   folder.addInput(params, "animate"); // Note that you are adding no options here unlike above, Tweakpane treats this as a boolean value
+
+  folder = pane.addFolder({ title: "Colour" });
+  folder.addInput(params, "fillStyle");
+  folder.addInput(params, "strokeStyle");
 };
 
 createPane();
