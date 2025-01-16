64ccf2270e17448a0dd27836fea0c4456ea577be
ShannyxMP <shannyxmp@gmail.com>
2025-01-16 16:01:04 +1000

    added animation to canvas-audio.js

--- sketches/sketch-audio.js
+++ sketches/sketch-audio.js
@@ -2,19 +2,34 @@ const canvasSketch = require("canvas-sketch");
 
 const settings = {
   dimensions: [1080, 1080],
+  animate: true,
 };
 
 // Global variables:
 let audio;
+let audioContext, audioData, sourceNode, analyserNode;
+let manager; // To control the sketch once audio finishes/pauses
 
 const sketch = () => {
-  audio = document.createElement("audio");
-  audio.src =
-    "./audio/Michael Napiza - Saturn x Slow Dancing x Pluto Projector.mp3";
-
   return ({ context, width, height }) => {
     context.fillStyle = "white";
     context.fillRect(0, 0, width, height);
+
+    if (!audioContext) return;
+
+    analyserNode.getFloatFrequencyData(audioData);
+
+    const avg = getAverage(audioData);
+
+    context.save();
+    context.translate(width * 0.5, height * 0.5);
+    context.lineWidth = 10;
+
+    context.beginPath();
+    context.arc(0, 0, Math.abs(avg), 0, Math.PI * 2);
+    context.stroke();
+
+    context.restore();
   };
 };
 
@@ -22,11 +37,52 @@ const addListeners = () => {
   window.addEventListener(
     "mouseup",
     () /* NOTE: this is an inline, anonymous function */ => {
-      if (audio.paused) audio.play();
-      else audio.pause();
+      if (!audioContext) createAudio();
+
+      if (audio.paused) {
+        audio.play();
+        manager.play();
+      } else {
+        audio.pause();
+        manager.pause();
+      }
     }
   );
 };
-addListeners();
 
-canvasSketch(sketch, settings);
+// To create an audio context and connect an analyser node to read frequency data from an audio source
+const createAudio = () => {
+  audio = document.createElement("audio");
+  audio.src =
+    "./audio/Michael Napiza - Saturn x Slow Dancing x Pluto Projector.mp3";
+
+  audioContext = new AudioContext();
+
+  sourceNode = audioContext.createMediaElementSource(audio);
+  sourceNode.connect(audioContext.destination);
+
+  analyserNode = audioContext.createAnalyser();
+  sourceNode.connect(analyserNode);
+
+  audioData = new Float32Array(analyserNode.frequencyBinCount);
+
+  console.log(audioData.length); // Same as 'FrequencyBinCount'
+};
+
+getAverage = (data) => {
+  let sum = 0;
+
+  for (let i = 0; i < data.length; i++) {
+    sum += data[i];
+  }
+
+  return sum / data.length;
+};
+
+const start = async () => {
+  addListeners();
+  manager = await canvasSketch(sketch, settings);
+  manager.pause();
+};
+
+start();
