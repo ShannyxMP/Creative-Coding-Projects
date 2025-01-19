56d301413e8d7bec0e2f02340df286799b832bde
ShannyxMP <shannyxmp@gmail.com>
2025-01-19 23:16:56 +1000

    updated sketch-particles.js to integrate images + added package

--- sketches/sketch-particles.js
+++ sketches/sketch-particles.js
@@ -3,6 +3,7 @@ const random = require("canvas-sketch-util/random");
 const math = require("canvas-sketch-util/math");
 const eases = require("eases");
 const colormap = require("colormap");
+const interpolate = require("color-interpolate");
 
 const settings = {
   dimensions: [1080, 1080],
@@ -18,14 +19,33 @@ const colors = colormap({
 });
 
 let elCanvas;
+let imgA, imgB;
 
 const sketch = ({ width, height, canvas }) => {
   let x, y, particle, radius;
-  let pos = [];
+  // let pos = [];
 
-  const numCircles = 15;
-  const gapCircle = 8;
-  const gapDot = 4;
+  const imgACanvas = document.createElement("canvas");
+  const imgAContext = imgACanvas.getContext("2d");
+
+  const imgBCanvas = document.createElement("canvas");
+  const imgBContext = imgBCanvas.getContext("2d");
+
+  imgACanvas.width = imgA.width;
+  imgACanvas.height = imgA.height;
+
+  imgBCanvas.width = imgB.width;
+  imgBCanvas.height = imgB.height;
+
+  imgAContext.drawImage(imgA, 0, 0);
+  imgBContext.drawImage(imgB, 0, 0);
+
+  const imgAData = imgAContext.getImageData(0, 0, imgA.width, imgA.height).data;
+  const imgBData = imgBContext.getImageData(0, 0, imgB.width, imgB.height).data;
+
+  const numCircles = 30;
+  const gapCircle = 2;
+  const gapDot = 2;
   let dotRadius = 12;
   let cirRadius = 0;
   const fitRadius = dotRadius;
@@ -53,6 +73,7 @@ const sketch = ({ width, height, canvas }) => {
     const circumference = Math.PI * 2 * cirRadius;
     const numFit = i ? Math.floor(circumference / (fitRadius * 2 + gapDot)) : 1;
     const fitSlice = (Math.PI * 2) / numFit;
+    let ix, iy, idx, r, g, b, colA, colB, colMap;
 
     for (let j = 0; j < numFit; j++) {
       const theta = fitSlice * j;
@@ -63,18 +84,38 @@ const sketch = ({ width, height, canvas }) => {
       x += width * 0.5;
       y += height * 0.5;
 
-      radius = dotRadius;
+      ix = Math.floor((x / width) * imgA.width);
+      iy = Math.floor((y / height) * imgA.height);
+      idx = (iy * imgA.width + ix) * 4;
 
-      particle = new Particle({ x, y, radius });
+      r = imgAData[idx + 0];
+      g = imgAData[idx + 1];
+      b = imgAData[idx + 2];
+      colA = `rgb(${r}, ${g}, ${b})`;
+
+      // radius = dotRadius;
+      radius = math.mapRange(r, 0, 255, 1, 12);
+
+      r = imgBData[idx + 0];
+      g = imgBData[idx + 1];
+      b = imgBData[idx + 2];
+      colB = `rgb(${r}, ${g}, ${b})`;
+
+      colMap = interpolate([colA, colB]);
+
+      particle = new Particle({ x, y, radius, colMap });
       particles.push(particle);
     }
 
     cirRadius += fitRadius * 2 + gapCircle;
     dotRadius = (1 - eases.quadOut(i / numCircles)) * fitRadius; // WITHOUT 'eases.quadOut()' it changes the size of the particle outwards, *linearly*
   }
+
   return ({ context, width, height }) => {
     context.fillStyle = "black";
     context.fillRect(0, 0, width, height);
+
+    context.drawImage(imgACanvas, 0, 0);
 
     particles.sort((a, b) => a.scale - b.scale); // Removing this line with layer particles based on when they're drawn in the loop
     // ^ Sorts larger particles to be at the top layer of the canvas
@@ -86,17 +127,17 @@ const sketch = ({ width, height, canvas }) => {
   };
 };
 
-const onMouseDown = (event) => {
+const onMouseDown = (e) => {
   window.addEventListener("mousemove", onMouseMove);
   window.addEventListener("mouseup", onMouseUp);
 
-  onMouseMove(event);
+  onMouseMove(e);
 };
 
-const onMouseMove = (event) => {
+const onMouseMove = (e) => {
   // Calculate the position of the cursor proportional to the scale of the sketch (refer to sketch-curves-intro.js)
-  const x = (event.offsetX / elCanvas.offsetWidth) * elCanvas.width;
-  const y = (event.offsetY / elCanvas.offsetHeight) * elCanvas.height;
+  const x = (e.offsetX / elCanvas.offsetWidth) * elCanvas.width;
+  const y = (e.offsetY / elCanvas.offsetHeight) * elCanvas.height;
 
   cursor.x = x; //? Why is this not declared in the particle class?
   cursor.y = y;
@@ -113,15 +154,31 @@ const onMouseUp = () => {
   cursor.y = 9999;
 };
 
-canvasSketch(sketch, settings);
+const loadImage = async (url) => {
+  return new Promise((resolve, reject) => {
+    const img = new Image();
+    img.onload = () => resolve(img);
+    img.onerror = () => reject();
+    img.src = url;
+  });
+};
+
+const start = async () => {
+  imgA = await loadImage("images/image-02.png");
+  imgB = await loadImage("images/image-01.png");
+
+  canvasSketch(sketch, settings);
+};
+
+start();
 
 class Particle {
-  constructor({ x, y, radius = 10 }) {
+  constructor({ x, y, radius = 10, colMap }) {
     // Initial Position:
     this.ix = x;
     this.iy = y;
 
-    // Position:
+    // Position
     this.x = x;
     this.y = y;
 
@@ -135,7 +192,8 @@ class Particle {
 
     this.radius = radius;
     this.scale = 1;
-    this.color = colors[0];
+    this.colMap = colMap;
+    this.color = colMap(0);
 
     this.minDist = random.range(100, 200);
     this.pushFactor = random.range(0.01, 0.02);
@@ -159,10 +217,10 @@ class Particle {
 
     this.scale = math.mapRange(dd, 0, 200, 1, 5);
 
-    idxColor = Math.floor(
-      math.mapRange(dd, 0, 200, 0, colors.length - 1, true)
-    );
-    this.color = colors[idxColor];
+    // idxColor = Math.floor(math.mapRange(dd, 0, 200, 0, colors.length - 1, true));
+    // this.color = colors[idxColor];
+
+    this.color = this.colMap(math.mapRange(dd, 0, 200, 0, 1, true));
 
     // Push force to the particle:
     dx = this.x - cursor.x;
@@ -191,10 +249,10 @@ class Particle {
   draw(context) {
     context.save();
     context.translate(this.x, this.y);
+    context.fillStyle = this.color;
 
     context.beginPath();
     context.arc(0, 0, this.radius * this.scale, 0, Math.PI * 2);
-    context.fillStyle = this.color;
     context.fill();
 
     context.restore();
